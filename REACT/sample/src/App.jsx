49c28a472a3a24c12d32d8677346248fd40427a6
PRAVEEN-PR-2004 <praveen2004ttp@gmail.com>
2024-09-16 14:51:33 +0530

    tailwind form

--- REACT/sample/src/App.jsx
+++ REACT/sample/src/App.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
   return (
-    <div className='bg-black'>
-      <h1 className='flex text-white'>rrr</h1>
-    </div>
-  )
-}
+    <>
+      <div className='flex items-center justify-center w-screen h-screen bg-body'>
+        <div className='flex justify-center w-1/5 border rounded-md shadow-lg shadow-indigo-500/40 h-1/2 bg-slate-100'>
+          <form className='flex flex-col justify-center gap-4'>
+        <h1 className='mb-4 text-2xl text-center bold bb-color'>LOGIN FORM</h1>
+            <input
+              type='text'
+              placeholder='Name'
+              className='h-[10%]  outline-none focus:border-b-2 rounded focus:border-purple-300 p-2'
+            />
+            <input
+              type='password'
+              placeholder='Password'
+              className='h-[10%]  outline-none focus:border-b-2 rounded focus:border-purple-300 p-2'
+            />
+            <input
+              type='email'
+              placeholder='Email'
+              className='h-[10%]  outline-none focus:border-b-2 rounded focus:border-purple-300 p-2'
+            />
+            <button class="bg-purple-300 hover:bg-purple-400 active:bg-purple-500 focus:outline-none focus:ring focus:ring-violet-300 rounded mt-3 h-[10%]">
+            
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
